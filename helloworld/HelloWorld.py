ac431cb5cc560a50906852b474f1c11a90204779
Dan <pud_2@hotmail.com>
2018-12-18 14:44:04 +0000

    another update

--- helloworld/HelloWorld.py
+++ helloworld/HelloWorld.py
@@ -4,15 +4,27 @@ class HelloWorld(object):
     def __init__(self, environ):
         self.environ = environ
         super(HelloWorld,self).__init__()
-        self.headers = []
+        self.headers = {"Content-Type":"text/html"
+        }
         
-
     def getContent(self):
         return "<html>Hello World</html>" # This should be rendered using Jinja or something else
 
+
+   def encodeHeaders(self,headers):
+        encodedHeaders = []
+        for k,values in sorted(headers.items()):
+            if not isinstance(values, list):
+                values = [values]
+            for v in values:
+                encodedHeaders.append((k,unicode(v).encode("utf-8")))
+        return encodedHeaders
+
+
     def main(self):
         content = self.getContent()
-        return (content,self.headers)
+        headers = self.headers
+        return (content,self.encodedHeaders(headers))
         
 def application(environ, start_response):
     klass = HelloWorld(environ)
